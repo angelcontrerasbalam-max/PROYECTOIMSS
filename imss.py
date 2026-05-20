daa9785a45db2ce54219cac5b80e0ca39a778def
angelcontrerasbalam-max <angelcontrerasbalam@gmail.com>
2026-05-19 22:25:05 -0600

    Update imss.py

--- imss.py
+++ imss.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+import plotly.express as px
 
 def main():
     # Set page configuration for a formal, executive style
